e865f454fef09788e8b15dd20410ba9ca88b4a39
rperez_coaat <rperez@coaat.es>
2023-04-01 20:16:44 +0200

    - Documentación de los comportamientos

--- src/main/java/es/uma/isia/sma/controller/behaviour/ComportamientoCentralitaRecepcionMensajesCoches.java
+++ src/main/java/es/uma/isia/sma/controller/behaviour/ComportamientoCentralitaRecepcionMensajesCoches.java
@@ -2,6 +2,7 @@ package es.uma.isia.sma.controller.behaviour;
 
 import es.uma.isia.sma.controller.AgenteControlTrafico;
 import es.uma.isia.sma.controller.EntornoUrbanoManager;
+import es.uma.isia.sma.controller.LoggerController;
 import es.uma.isia.sma.model.celdas.Celda;
 import es.uma.isia.sma.model.celdas.CeldaTransitable;
 import es.uma.isia.sma.model.celdas.Semaforo;
@@ -12,15 +13,32 @@ import jade.lang.acl.MessageTemplate;
 import jade.lang.acl.UnreadableException;
 
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+/**
+ * Comportamiento que se ejecuta en el agente de control de tráfico para recibir y procesar
+ * los mensajes de los vehículos que solicitan avanzar en el entorno de simulación.
+ */
 public class ComportamientoCentralitaRecepcionMensajesCoches extends SimpleBehaviour {
+    private static final Logger logger = LoggerController.getInstance().getLogger();
+
     private final AgenteControlTrafico agenteControlTrafico;
     private boolean done = false;
 
+    /**
+     * Crea un nuevo comportamiento de recepción de mensajes de vehículos.
+     *
+     * @param agenteControlTrafico El agente de control del tráfico que ejecutará este comportamiento.
+     */
     public ComportamientoCentralitaRecepcionMensajesCoches(AgenteControlTrafico agenteControlTrafico) {
         this.agenteControlTrafico = agenteControlTrafico;
     }
 
+    /**
+     * Espera a recibir un mensaje de un vehículo y luego lo procesa para determinar si puede avanzar en el entorno
+     * de simulación.
+     */
     @Override
     public void action() {
         MessageTemplate mt = MessageTemplate.and(
@@ -29,40 +47,52 @@ public class ComportamientoCentralitaRecepcionMensajesCoches extends SimpleBehav
         );
         ACLMessage msg = agenteControlTrafico.receive(mt);
         if (msg != null) {
-            // Procesar el mensaje de AgenteCoche
-            try {
-                CeldaTransitable celdaActual = (CeldaTransitable) msg.getContentObject();
-                CeldaTransitable siguienteCelda = EntornoUrbanoManager.getInstance().getSiguienteCeldaTransitable(celdaActual);
-                if (siguienteCelda != null && !agenteControlTrafico.esPosicionOcupada(siguienteCelda.getCoordenadas())) {
-                    // Si la siguiente celda es un semáforo, verifica si la dirección permitida es la actual
-                    if (esSemaforoConDireccionPermitida(siguienteCelda, celdaActual)) {
-                        System.out.println("Es un semaforo y la dirección está permitida");
-                        liberarCeldaActual(celdaActual);
-                        ocuparSiguienteCelda(siguienteCelda);
-                        enviarMensajeAvance(msg, siguienteCelda);
-                    } else if (!(siguienteCelda instanceof Semaforo)) {
-                        liberarCeldaActual(celdaActual);
-                        ocuparSiguienteCelda(siguienteCelda);
-                        enviarMensajeAvance(msg, siguienteCelda);
-                    } else {
-                        enviarMensajeRechazoAvance(msg);
-                    }
-                } else {
-                    enviarMensajeRechazoAvance(msg);
-                }
-
-            } catch (UnreadableException e) {
-                // throw new RuntimeException(e);
-            } catch (IOException e) {
-                // throw new RuntimeException(e);
-            }
-
+            procesarMensajeVehiculo(msg);
             done = false;
         } else {
             block();
         }
     }
 
+    /**
+     * Procesa el mensaje recibido de un vehículo para determinar si puede avanzar en el entorno de simulación.
+     *
+     * @param msg El mensaje recibido de un vehículo.
+     */
+    private void procesarMensajeVehiculo(ACLMessage msg) {
+        try {
+            CeldaTransitable celdaActual = (CeldaTransitable) msg.getContentObject();
+            CeldaTransitable siguienteCelda = EntornoUrbanoManager.getInstance().getSiguienteCeldaTransitable(celdaActual);
+            if (siguienteCelda != null && !agenteControlTrafico.esPosicionOcupada(siguienteCelda.getCoordenadas())) {
+                // Si la siguiente celda es un semáforo, verifica si la dirección permitida es la actual
+                if (esSemaforoConDireccionPermitida(siguienteCelda, celdaActual)) {
+                    logger.info("Es un semaforo y la dirección está permitida");
+                    liberarCeldaActual(celdaActual);
+                    ocuparSiguienteCelda(siguienteCelda);
+                    enviarMensajeAvance(msg, siguienteCelda);
+                } else if (!(siguienteCelda instanceof Semaforo)) {
+                    liberarCeldaActual(celdaActual);
+                    ocuparSiguienteCelda(siguienteCelda);
+                    enviarMensajeAvance(msg, siguienteCelda);
+                } else {
+                    enviarMensajeRechazoAvance(msg);
+                }
+            } else {
+                enviarMensajeRechazoAvance(msg);
+            }
+
+        } catch (UnreadableException | IOException e) {
+            logger.log(Level.WARNING, "Error al procesar mensaje vehiculo ", e);
+        }
+    }
+
+    /**
+     * Envia un mensaje de acuerdo al avance del vehículo en el mapa.
+     *
+     * @param mensaje        el mensaje original enviado por el vehículo.
+     * @param siguienteCelda la celda a la que se va a avanzar.
+     * @throws IOException si ocurre un error al intentar enviar el mensaje.
+     */
     private void enviarMensajeAvance(ACLMessage mensaje, CeldaTransitable siguienteCelda) throws IOException {
         ACLMessage respuesta = mensaje.createReply();
         respuesta.setPerformative(ACLMessage.AGREE);
@@ -71,6 +101,11 @@ public class ComportamientoCentralitaRecepcionMensajesCoches extends SimpleBehav
         agenteControlTrafico.send(respuesta);
     }
 
+    /**
+     * Envia un mensaje de rechazo cuando el vehículo no puede avanzar.
+     *
+     * @param mensaje el mensaje original enviado por el vehículo.
+     */
     private void enviarMensajeRechazoAvance(ACLMessage mensaje) {
         ACLMessage reply = mensaje.createReply();
         reply.setPerformative(ACLMessage.REFUSE);
@@ -80,6 +115,13 @@ public class ComportamientoCentralitaRecepcionMensajesCoches extends SimpleBehav
     }
 
 
+    /**
+     * Verifica si la siguiente celda es un semáforo y si la dirección de movimiento está permitida.
+     *
+     * @param siguienteCelda la celda a la que se va a avanzar.
+     * @param celdaActual    la celda en la que se encuentra actualmente el vehículo.
+     * @return true si el semáforo permite avanzar en la dirección actual, false en caso contrario.
+     */
     private boolean esSemaforoConDireccionPermitida(CeldaTransitable siguienteCelda, CeldaTransitable celdaActual) {
         if (siguienteCelda instanceof Semaforo) {
             Semaforo semaforo = (Semaforo) siguienteCelda;
@@ -88,16 +130,24 @@ public class ComportamientoCentralitaRecepcionMensajesCoches extends SimpleBehav
         return false;
     }
 
+    /**
+     * Libera la celda actual en la que se encuentra el vehículo, la marca en la matriz como libre o transitable
+     *
+     * @param celdaActual la celda en la que se encuentra actualmente el vehículo.
+     */
     private void liberarCeldaActual(Celda celdaActual) {
-        if (celdaActual != null) {
+        if (celdaActual != null)
             agenteControlTrafico.marcarPosicion(celdaActual.getCoordenadas(), false);
-        }
     }
 
+    /**
+     * Marca como ocupada la siguiente celda a la que se va a avanzar.
+     *
+     * @param siguienteCelda la celda a la que se va a avanzar.
+     */
     private void ocuparSiguienteCelda(Celda siguienteCelda) {
         agenteControlTrafico.marcarPosicion(siguienteCelda.getCoordenadas(), true);
     }
-
 
     @Override
     public boolean done() {
--- src/main/java/es/uma/isia/sma/controller/behaviour/ComportamientoCentralitaRecepcionMensajesSemaforos.java
+++ src/main/java/es/uma/isia/sma/controller/behaviour/ComportamientoCentralitaRecepcionMensajesSemaforos.java
@@ -1,20 +1,39 @@
 package es.uma.isia.sma.controller.behaviour;
 
 import es.uma.isia.sma.controller.AgenteControlTrafico;
+import es.uma.isia.sma.controller.LoggerController;
 import es.uma.isia.sma.model.celdas.Semaforo;
 import jade.core.behaviours.SimpleBehaviour;
 import jade.lang.acl.ACLMessage;
 import jade.lang.acl.MessageTemplate;
 import jade.lang.acl.UnreadableException;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Comportamiento que se encarga de recibir los mensajes enviados por los agentes semáforo
+ * y actualizar la dirección permitida en el agente control de tráfico.
+ */
 public class ComportamientoCentralitaRecepcionMensajesSemaforos extends SimpleBehaviour {
+    private static final Logger logger = LoggerController.getInstance().getLogger();
+
     private final AgenteControlTrafico agenteControlTrafico;
     private boolean done = false;
 
+    /**
+     * Crea un nuevo comportamiento de recepción de mensajes de semáforos.
+     *
+     * @param agenteControlTrafico agente control de tráfico al que pertenece este comportamiento.
+     */
     public ComportamientoCentralitaRecepcionMensajesSemaforos(AgenteControlTrafico agenteControlTrafico) {
         this.agenteControlTrafico = agenteControlTrafico;
     }
 
+    /**
+     * Recibe los mensajes enviados por los agentes semáforo y actualiza la dirección permitida
+     * en el agente control de tráfico.
+     */
     @Override
     public void action() {
         // Como enfoque alternativo a MessageTemplate.MatchContent, envío el objeto Semáforo directamente como contenido
@@ -32,18 +51,21 @@ public class ComportamientoCentralitaRecepcionMensajesSemaforos extends SimpleBe
         );
         ACLMessage msg = agenteControlTrafico.receive(mt);
         if (msg != null) {
-            // Procesar el mensaje de AgenteSemaforo
             try {
                 Semaforo semaforo = (Semaforo) msg.getContentObject();
                 agenteControlTrafico.actualizarDireccionPermitidaSemaforo(semaforo);
             } catch (UnreadableException e) {
-             //   System.err.println(e.getMessage());
+                logger.log(Level.WARNING, "Error al obtener el contenido del semaforo ", e);
             }
         } else {
             block();
         }
     }
 
+    /**
+     * Indica si el comportamiento ha finalizado su ejecución.
+     * @return true si el comportamiento ha finalizado, false en caso contrario.
+     */
     @Override
     public boolean done() {
         return done;
